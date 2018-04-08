44fc429a6c8a7d0dc9b97b7387c8f97797cdb138
DeathCode <carotkut12@gmail.com>
2018-04-08 17:11:27 +0530

    imported R

--- app/src/main/java/com/death/paper/activity/NewsArticle.java
+++ app/src/main/java/com/death/paper/activity/NewsArticle.java
@@ -3,6 +3,8 @@ package com.death.paper.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.death.paper.R;
+
 public class NewsArticle extends AppCompatActivity {
 
     @Override
